74033e16e606df14148bc91a3e47bb923f0078b6
Vi-Sri <nikola.tesla195@gmail.com>
2023-10-10 20:34:50 -0400

    fixes in pacl

--- core.py
+++ core.py
@@ -9,19 +9,19 @@ class VisionTransformerWrapper(nn.Module):
         self.vision_transformer = vision_transformer
 
     def forward(self, x: torch.Tensor):
-        x = self.conv1(x)  
+        x = self.vision_transformer.conv1(x)  
         x = x.reshape(x.shape[0], x.shape[1], -1)  
         x = x.permute(0, 2, 1)  
-        x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
-        x = x + self.positional_embedding.to(x.dtype)
-        x = self.ln_pre(x)
+        x = torch.cat([self.vision_transformer.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)
+        x = x + self.vision_transformer.positional_embedding.to(x.dtype)
+        x = self.vision_transformer.ln_pre(x)
 
         x = x.permute(1, 0, 2)  
-        x = self.transformer(x)
+        x = self.vision_transformer.transformer(x)
         x = x.permute(1, 0, 2)  
 
-        x = self.ln_post(x[:, 1:, :])
-        return x 
+        x = self.vision_transformer.ln_post(x[:, 1:, :])
+        return x
         
 
 class PACLVisionEncoder(nn.Module):
